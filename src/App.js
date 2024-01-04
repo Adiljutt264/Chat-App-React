dc4608ffe7e8d0b80ace0854f788e9b80ae26470
Adiljutt264 <adiljutt.264@gmail.com>
2024-01-05 01:32:34 +0500

    Firebase snapshots display messages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,68 @@
 import './App.css';
-import { Box, Container, VStack, Button, Input, HStack } from '@chakra-ui/react';
-import Message from './Components/Message';
-import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "./Firebase";
 import { useEffect, useState } from 'react';
+import { Box, Container, VStack, Button, Input, HStack } from '@chakra-ui/react';
+import Message from './Components/Message';
+import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, addDoc, collection, serverTimestamp, onSnapshot} from "firebase/firestore";
 const loginHandler = ()=>{
   const provider = new GoogleAuthProvider();
   signInWithPopup( auth, provider)
 }
+const logoutHandler =()=> signOut(auth);
   const auth = getAuth(app);
+  const  db = getFirestore(app);
 function App() {
   const [user, setUser] = useState(false);
-  useEffect(() => {
-    onAuthStateChanged(auth, (data)=>{
-      console.log(data);
-    })
-    return () => {
-    
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const submitHandler = async (e)=>{
+    e.preventDefault();
+    try {
+      await addDoc(collection(db , "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp()
+      });
+      setMessage("");
+    } catch (error) {
+      console.error("Firestore Error:", error);
+      alert(error.message); // Display the error message
     }
-  }, [])
+  }
+  useEffect(() => {
+    const unSubscribeForMessage = onSnapshot(collection(db, "Messages") , (snap)=>{
+      setMessages(snap.docs.map((item) => {
+        const id = item.id;
+        return{id, ...item.data()}
+      }));
+    });
+    const unSubscribe = onAuthStateChanged(auth, (data)=>{
+      setUser(data);
+    });
+    return()=>{
+      unSubscribe();
+      unSubscribeForMessage();
+    };
+  });
   
   return (
     <Box bg={"red.50"}>
-    {user? ( <Container h={"100vh"} bg={"white"}>
+    { user ? ( <Container h={"100vh"} bg={"white"}>
       <VStack h="full" padding={"4"}>
-        <Button w={"full"} colorScheme={"red"}>Logout</Button>
+        <Button onClick={logoutHandler} w={"full"} colorScheme={"red"}>Logout</Button>
         <VStack background={"white"} h="full" w={"full"} overflowY={"auto"} >
-        <Message text={"Sample Text"}/>
-        <Message user="me" text={"Sample text two"}/>
-        <Message text={"Sample Text three"}/>
-        <Message text={"Sample Text three"}/>
-        <Message text={"Sample Text three"}/>
-        <Message user="me" text={"Sample text two"}/>
-        <Message user="me" text={"Sample text two"}/>
-        <Message user="me" text={"Sample text two"}/>
+        {
+          messages.map((item) => (
+          <Message key={item.id} user={ item.uid === user.uid ? "me" : "other"} text={item.text} uri={item.uri}/>
+        ))
+        }
         </VStack>
-        <form style={{width:"100%"}}>
+        <form onSubmit={submitHandler} style={{width:"100%"}}>
         <HStack h="full" w={"full"}>
-        <Input placeholder='Enter a Message'/>
-        <Button colorScheme={"purple"}>Send</Button>
+        <Input value={message} onChange={(e)=>setMessage(e.target.value)} placeholder='Enter a Message'/>
+        <Button  type='submit' colorScheme={"purple"}>Send</Button>
         </HStack>
         </form>
       </VStack>
